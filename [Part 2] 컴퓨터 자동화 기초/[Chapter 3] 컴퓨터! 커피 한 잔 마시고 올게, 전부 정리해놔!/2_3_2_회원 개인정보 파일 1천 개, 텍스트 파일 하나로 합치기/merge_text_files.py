4972c15b7037743e2385a9b6392208878d842d50
bcheollee <135472050+bcheollee@users.noreply.github.com>
2024-12-01 11:03:29 +0900

    Add files via upload

--- [Part 2] 컴퓨터 자동화 기초/[Chapter 3] 컴퓨터! 커피 한 잔 마시고 올게, 전부 정리해놔!/2_3_2_회원 개인정보 파일 1천 개, 텍스트 파일 하나로 합치기/merge_text_files.py
+++ [Part 2] 컴퓨터 자동화 기초/[Chapter 3] 컴퓨터! 커피 한 잔 마시고 올게, 전부 정리해놔!/2_3_2_회원 개인정보 파일 1천 개, 텍스트 파일 하나로 합치기/merge_text_files.py
@@ -6,51 +6,42 @@ Last Modification : 2024.12.01.
 """
 
 import time
-import os
 import logging
 from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def read_file(file_path):
+    try:
+        with file_path.open('r', encoding='utf-8') as file:
+            return file.read() + "\n\n"
+    except IOError as e:
+        logging.error(f"{file_path} 파일 읽기 오류: {e}")
+        return ""
 
 def merge_files(directory, outfile_name):
-    # 작업 시작 메시지를 로깅합니다.
     logging.info("프로세스 시작")
-
     start_time = time.time()
 
     directory_path = Path(directory)
     outfile_path = Path(outfile_name)
 
     try:
-        # 결과물 파일을 생성합니다.
         with outfile_path.open('w', encoding='utf-8') as out_file:
-            # 폴더의 내용물을 열람해 목록을 생성합니다.
-            for file_path in directory_path.glob('*.txt'):
-                try:
-                    # 텍스트 파일을 읽어옵니다.
-                    with file_path.open('r', encoding='utf-8') as file:
-                        content = file.read()
-                        # 파일의 내용물을 결과물 파일에 기재합니다.
-                        out_file.write(content + "\n\n")
-                except IOError as e:
-                    logging.error(f"{file_path} 파일 읽기 오류: {e}")
+            with ThreadPoolExecutor() as executor:
+                futures = [executor.submit(read_file, file_path) for file_path in directory_path.glob('*.txt')]
+                for future in as_completed(futures):
+                    out_file.write(future.result())
 
         logging.info("프로세스 완료")
-
         end_time = time.time()
         logging.info(f"작업 소요 시간: {end_time - start_time:.2f}초")
 
     except IOError as e:
         logging.error(f"결과 파일 {outfile_name} 쓰기 오류: {e}")
 
-
 if __name__ == "__main__":
-    # 로깅 설정
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    # 하나로 합칠 파일들이 저장된 폴더 이름을 적어주세요.
     directory = "personal_info"
-
-    # 결과물 파일의 이름을 정의합니다.
     outfile_name = "merged_ID.txt"
-
     merge_files(directory, outfile_name)
